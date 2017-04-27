35ef5f8248946acaa02925a31978e7dab314a45e
Jacob Nelson <jnelson3225@gmail.com>
2017-04-27 13:36:12 -0400

    probe.c debug

--- probe.c
+++ probe.c
@@ -1,5 +1,13 @@
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define LLC_SIZE 6144000 // Bytes
+#define LLC_WAYS 12 // ways
+#define LLC_SETS 8192 // sets
+#define LLC_LINESIZE 64 // Bytes
+
+volatile void* chase_global; // used to defeat optimizations (not sure why!)
 
 inline unsigned long long rdtscp() {
   unsigned int lo, hi;
@@ -11,43 +19,110 @@ inline unsigned long long rdtscp() {
   return ((unsigned long long)lo) | (((unsigned long long)hi) << 32);
 }
 
-int main(){
-
-  /*
-    For cache size we need to determine the number of bits in the set.
-    Depending on the type of cace we're using we need to determine the number
-    of sets in the cache. For two-way associative we have 2 cache blocks and a
-    block contains 16 bytes. There fore a set for 2-way has 32 bytes
-  */
-
-	//LLC_size = 32 *1024 (32K);
-	//stride = LLC_sets? * cacheline_sz (64 bytes);
-  //cacheline_sz = 64 bytes because of x86 processor;
-	//buffer = alloc_ptr_chasing_buff(size, stride)?;
-unsigned long long start_t, end_t, total_t, average = 0;
-   int i, j;
-
-   printf("Starting of the program, start_t = %llu\n", start_t);
-
-   printf("Going to scan a big loop, start_t = %llu\n", start_t);
-   for(j = 0; j < 10; j++){
-   	start_t = rdtscp();
-   		for(i=0; i< 1000000000; i++)
-   		{
-   		}
-   		end_t = rdtscp();
-   		total_t = (1000*(end_t-start_t)/(unsigned long long)CLOCKS_PER_SEC);
-   		average += total_t;
-   		printf("Run %d: %llu\n", j, total_t);
+unsigned int log_base2(unsigned int x) {
+	unsigned int count = 0;
+	while(x >>= 1){
+		++count;
 	}
-	printf("Average: %llu\n", average/10);
-   
-   //RDTSC()
-   //printf("End of the big loop, end_t = %llu\n", end_t);
-
-   return(0);
+	return count;
 }
 
+unsigned int bit_reverse(unsigned int val, unsigned int bits) {
+	unsigned int result = 0;
+	while(bits > 0) {
+		result = (result << 1) | (val & 1);
+		val >>= 1;
+		--bits;
+	}
+	return result;
+}
+
+void generate_bit_rev_perm(unsigned int **dst, unsigned int bits, unsigned int count) {
+	unsigned int maxval = 1 << bits;
+	unsigned int i;
+	for(i = 0; i < maxval; i++) {
+		*dst[i] = bit_reverse(i, bits);
+	}
+
+	unsigned int current = maxval;
+	unsigned int index = 0;
+	while(current > count) {
+		while(*dst[index] < count) ++index;
+		--current;
+		*dst[index] = *dst[current];	
+	}
+}
+
+void** alloc_ptr_chasing_buf(size_t size, size_t stride) {
+	size_t buf_len = size / sizeof(void*);  
+	void** mem = (void**) malloc(size); // allocate a buffer to be the correct size, returned to caller
+	int i;
+	/*for(i = 0; i < buf_len; i++) {
+		mem[i] = (void*) malloc(sizeof(void*));
+		}*/
+	
+	unsigned int runs = stride / sizeof(void*);
+	unsigned int bits = log_base2(runs);
+	if((1<<bits)!=runs) bits++;
+	unsigned int *offset[1<<bits];
+	for(i = 0; i < 1<<bits; i++) {
+		offset[i] = (unsigned int*) malloc(sizeof(unsigned int));
+	}
+
+	generate_bit_rev_perm(offset, bits, runs);
+
+	unsigned int run;
+	void** last = NULL;
+	for(run = 0; run < runs; run++) {
+		printf("run %d\n", run);
+		char* next = (char*)mem + *(offset[run]) * sizeof(void*);
+		if(last) {
+			*last = (void*) next;		
+		}
+		last = (void**) next;
+		for(;;) {
+			char* next = (char*)last + stride;
+			if(next >= (char*)mem + size) break;
+			*last = (void*) next;
+			last = (void**) next;
+		}
+	}
+
+	// free allocated memory for offset
+	for(i = 0; i < 1<<bits; i++) {
+	  free(offset[i]);
+	}
+
+	*last = (void*) mem;
+	return (void**) mem;
+}
+
+int main() {
+	unsigned int size = LLC_SIZE * (LLC_WAYS + 1);
+	unsigned int stride = LLC_SETS * LLC_LINESIZE;
+	void** chasing_buf = alloc_ptr_chasing_buf(size, stride);
+	
+	unsigned long long results[10];
+	int i, j;
+	for(i = 0; i < 10; i++) {
+		printf("...probe %d started\n", i);
+		unsigned long long start = rdtscp();
+		void** current = (void**) chasing_buf;
+		for(j = 0; j < 10000; j++) {
+			current = (void**) *current; 
+		}
+		chase_global = *current;
+		results[i] = (rdtscp() - start);
+		printf("...probe %d stopped\n", i);
+	}
+
+	for(i = 0; i < 10; i++) {
+		printf("Run %d: %llu\n", i, results[i]);
+	}
+	
+	// free all pointers
+	free(chasing_buf);
+}
 
 /*
 Normal output
