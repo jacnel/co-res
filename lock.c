dec053b438611b74c5c4e1b98bc52ad3c9c421c3
Jacob Nelson <jnelson3225@gmail.com>
2017-05-01 21:52:12 -0400

    multithreaded lock

--- lock.c
+++ lock.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdatomic.h>
+#include <pthread.h>
 
 #define LENGTH 8192
 
-int main() {
-	atomic_char* ptr = (atomic_char*) malloc((LENGTH + 1) * 8);
-	atomic_int* unalgn_ptr = ptr + 2;
-
-	while(1) {
-		int i;
-		for(i = 0; i < LENGTH; i++) {
-			atomic_fetch_add(unalgn_ptr + i, 1);
-		}
-	}
-
-	return 0;
+void * thread_func(void* unalgn_ptr) {
+  int i;
+  for(i = 0; i < LENGTH; i++) {
+    atomic_fetch_add(*((atomic_int**)(unalgn_ptr)) + i, 1);
+  }
+  return NULL;
 }
+
+int main() {
+  atomic_char* ptr = (atomic_char*) malloc((LENGTH + 1) * 8);
+  atomic_int* unalgn_ptr = (atomic_int*) (ptr + 2);
+  
+  while(1) {
+    pthread_t threads[50];
+    int i;
+    for(i = 0; i  < 50; ++i) {
+      pthread_create(&threads[i], NULL, thread_func, &unalgn_ptr);
+    }
+    for(i = 0; i < 50; ++i) {
+      pthread_join(threads[i], NULL);
+    }
+  }
+  
+  return 0;
+}
+
 
 /*
 Without Locking:
